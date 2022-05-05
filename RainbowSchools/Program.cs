56665511c18e312809c61759215de057208a8dbc
Venkata K <venkata.kondepudi@dell.com>
2022-05-04 20:20:57 -0400

    Initial Commit

---
+++ RainbowSchools/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+doWork();
+
+void doWork()
+{
+    string [] studentdetails = File.ReadAllLines(@"C:\Venkata\trunk\simplilearn\section2.1.9\RainbowSchools\data\student.txt");
+    foreach(string studentdetail in studentdetails)
+    {
+        Console.WriteLine(studentdetail);
+    }
+}
